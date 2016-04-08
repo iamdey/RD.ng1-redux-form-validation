eb61b3fbb13b31b8c4a64c3aa153ddd473665d42
David Epely <esion99@gmail.com>
2016-04-08 16:17:49 +0200

    signup form

--- index.js
+++ index.js
@@ -2,16 +2,34 @@ import {module} from 'angular';
 
 const app = module('myApp', []);
 
-class LoginFormController {
+class SignUpFormController {
   constructor() {
     console.log('logforrm ctrl');
   }
 }
 
-const LoginFormComponent = {
-  bindings: {},
-  controller: LoginFormController,
-  template: `<div>Foo</div>`,
-}
+app.component('signupForm', {
+  bindings: {
+    list: '<',
+  },
+  controller: SignUpFormController,
+  template: `
+    <form ng-submit="$ctrl.signup() name="signup">
+      <p>form for {{ $ctrl.list.name }}</p>
+      <div class="form-group">
+        <label>email</label>
+        <input type="email" required />
+      </div>
+      <div>
+        <button type="submit" class="btn btn-primary"><i class="fa fa-add"></i>Add</submit>
+      </div>
+    </form>`,
+});
 
-app.component('loginForm', LoginFormComponent);
+app.component('page', {
+  template: `
+    <h1>Welcome and signup OKay?!</h1>
+    <h2>Sign Up to <trong>A</strong></h2>
+    <signup-form list="{name: 'A', id: 'a'}"></signup-form>
+  `
+});
